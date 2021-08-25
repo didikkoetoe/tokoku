05047ffe0fada4fe03eaa3da4222e7b634ae8e24
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 06:50:19 +0700

    Config untuk database

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/mvc/public');
+
+// Database
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'vsga');
