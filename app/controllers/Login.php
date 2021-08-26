6db0d56ce165269f0eccbc38523c92fe3312f182
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 09:34:06 +0700

    Menambahkan method login untuk admin

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -9,19 +9,27 @@ class Login extends Controller
 
     public function index()
     {
-        // if ($this->model('Admin_model')->getAdmin($_POST) > 0) {
-        //     header('Location: ' . BASEURL . '/Admin');
-        //     exit;
-        // } else {
-        //     Flasher::setFlash('Username / Password', 'salah', 'success');
-        //     header('Location: ' . BASEURL . '/Login');
-        //     exit;
-        // }
-
         $data = [
             'title' => 'Login',
         ];
 
         $this->view('login/login', $data);
+    }
+
+    public function login()
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if ($this->model('Admin_model')->getAdmin($username, $password) > 0) {
+            Flasher::setFlash('Selamat datang Admin', 'semoga harimu menyenangkan', 'success');
+            $_SESSION['admin'] = 'Didik Prabowo';
+            header('Location: ' . BASEURL . '/Admin');
+            exit;
+        } else {
+            Flasher::setFlash('Username / Password', 'salah, coba lagi', 'danger');
+            header('Location: ' . BASEURL . '/Login');
+            exit;
+        }
     }
 }
