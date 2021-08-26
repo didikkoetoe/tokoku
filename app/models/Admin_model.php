592d0d0c1eec3c0dc4008dd1b8963a8ebc37d54a
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 08:38:44 +0700

    Menambahkan model untuk admin

---
+++ app/models/Admin_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Admin_model
+{
+    private $table = 'admin';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getAdmin()
+    {
+        $this->db->query("SELECT * FROM {$this->table}");
+
+        // return $this->db->single();
+    }
+}
