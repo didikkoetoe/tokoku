8b932b08b424ad69edaf4f5e3ada53010f966fe0
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 14:42:47 +0700

    Menambahkan pengecekan ke password

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -2,17 +2,19 @@
 
 class User extends Controller
 {
-    public function index()
-    {
-        $data = [
-            'title' => 'Daftar Akun'
-        ];
-
-        $this->view('user/index');
-    }
-
     public function tambahUser()
     {
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+
+        // if($this->model('User_model')->)
+
+        if ($password !== $password2) {
+            Flasher::setFlash('Password tidak sama', ', ulangi lagi', 'danger');
+            header('Location: ' . BASEURL . '/Login');
+            exit;
+        }
+
         if ($this->model('User_model')->tambahUser($_POST) > 0) {
             Flasher::setFlash('Registri akun', 'berhasil', 'success');
             header('Location: ' . BASEURL . '/Login');
