9c88a8995cd8beb8a8a64b3ec89d5ae89fa6c8f5
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 12:23:49 +0700

    Ubah alert agar di tengah

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -14,7 +14,7 @@ class Flasher
     public static function flash()
     {
         if (isset($_SESSION['flash'])) {
-            echo '<div class="alert alert-' . $_SESSION['flash']['type'] . ' alert-dismissible fade show" role="alert">
+            echo '<div class="text-center alert alert-' . $_SESSION['flash']['type'] . ' alert-dismissible fade show" role="alert">
             <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>';
