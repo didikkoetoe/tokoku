bb107a2a6a0e929feaab61d6b55d31620f338214
didikkoetoe <danieldidikprabowo@gmail.com>
2021-08-26 11:34:17 +0700

    Ajax untuk edit data produk

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    $('.tombolTambah').click(function (e) { 
+        e.preventDefault();
+        
+        $('#formModalLabel').html('Tambah Data Produk');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+
+        $('#nama').val('');
+        $('#harga').val('');
+        $('#berat').val('');
+        $('#kategori').val('All');
+        $('#deskripsi').val('');
+        $('#gambar').val('');
+        $('#id').val('');
+    });
+    
+    $('.edit').click(function (e) { 
+        e.preventDefault();
+        
+        $('#formModalLabel').html('Ubah Data Produk');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('action', 'http://localhost/tokoku/public/Admin/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            type: "post",
+            url: "http://localhost/tokoku/public/Admin/getUbah",
+            data: {id : id},
+            dataType: "json",
+            success: function (response) {
+                $('#nama').val(response.nama);
+                $('#harga').val(response.harga);
+                $('#berat').val(response.berat);
+                $('#kategori').val(response.kategori);
+                $('#deskripsi').val(response.deskripsi);
+                $('#gambar').val(response.gambar);
+                $('#id').val(response.id);
+            }
+        });
+    });
+    
+});
